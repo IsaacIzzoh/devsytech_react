9822bbe268c9ac0c10f87aeaf77a24c195ed5cdd
IsaacIzzoh <124176530+IsaacIzzoh@users.noreply.github.com>
2024-03-29 23:19:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css";
 import "font-awesome/css/font-awesome.css";
